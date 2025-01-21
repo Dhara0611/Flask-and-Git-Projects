0427860deb6492625a3b5ef6e621922c51f02648
Dhara Deshpande <Dhara@Kishans-MacBook-Air.local>
2025-01-21 13:52:47 -0800

    updated user to username

--- resources/user.py
+++ resources/user.py
@@ -19,7 +19,7 @@ class UserRegister(MethodView):
     def post(self, user_data):
         
         user = UserModel(
-            user = user_data["username"],
+            username = user_data["username"],
             password = pbkdf2_sha256.hash(user_data["password"])
         )
         try:
